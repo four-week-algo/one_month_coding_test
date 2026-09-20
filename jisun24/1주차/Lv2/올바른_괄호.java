a431c779a9d9c4ca741e7c79d6f5fab4339862ac
jisun24 <jsryu2043@gmail.com>
2026-09-20 18:15:36 +0900

    올바른_괄호.java

---
+++ jisun24/1주차/Lv2/올바른_괄호.java
@@ -0,0 +1,21 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+import java.util.*;
+
+class Solution{
+    public boolean solution(String s){
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(c == '('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
