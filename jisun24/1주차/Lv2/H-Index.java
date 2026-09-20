fff816016b7d4817cdcadfe550faccd990d84ee7
jisun24 <jsryu2043@gmail.com>
2026-09-20 19:36:30 +0900

    H-Index.java

---
+++ jisun24/1주차/Lv2/H-Index.java
@@ -0,0 +1,20 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42747
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        
+        int n = citations.length;
+        
+        for(int i = 0; i < n; i++){
+            int h = n - i;
+            
+            if(citations[i] >= h){
+                return h;
+            }
+        }
+        return 0;
+    }
+}
